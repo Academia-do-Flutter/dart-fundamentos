03db57cda9006cefde731d1993cac7edcda73b1b
renatorv <renatorv@gmail.com>
2022-07-10 22:36:32 -0300

    finalizado aula do for e for in

--- lib/6Loops/foForin.dart
+++ lib/6Loops/foForin.dart
@@ -1,14 +1,37 @@
+// ignore: file_names
 void main() {
   var numeros = List.generate(10, (index) => index);
   var nomes = ['Neide', 'Neuza', 'Regina', 'Rejane', 'Hanna', 'Júlia'];
 
-  print('Imprimeindo números com for convencional');
-  for (var i = 0; i < numeros.length; i++) {
-    print(numeros[i]);
-  }
+  // print('Imprimindo números com for convencional');
+  // for (var i = 0; i < numeros.length; i++) {
+  //   print(numeros[i]);
+  // }
 
-    print('Imprimeindo nomes com for convencional');
+  // print('Imprimindo nomes com for convencional');
+  // for (var i = 0; i < nomes.length; i++) {
+  //   print(nomes[i]);
+  // }
+
+  // FOR IN
+  for (var numero in numeros) {
+    print(numero);
+  }
+  print('\n');
+
+  // BREAK
   for (var i = 0; i < nomes.length; i++) {
+    print(nomes[i]);
+    if (nomes[i] == 'Regina') {
+      break;
+    }
+  }
+  print('\n');
+  // CONTINUE
+  for (var i = 0; i < nomes.length; i++) {
+    if (nomes[i] == 'Regina') {
+      continue;
+    }
     print(nomes[i]);
   }
 }
